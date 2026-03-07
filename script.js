84d286ea7eab64d131fcad4d9a9cf18b56c34c10
Raja iblis <ytamingamers@gmail.com>
2026-03-07 08:40:03 +0700

    display table

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fileInput = document.getElementById("inputFile");
+let output = document.getElementById('output');
 fileInput.addEventListener("change", async function(){
     const file = this.files[0];
     const zip = await JSZip.loadAsync(file);
@@ -17,15 +18,42 @@ fileInput.addEventListener("change", async function(){
       data1.push(name.string_list_data[0]['value'])
     })
     console.log(data1)
-    
     let data2 = []
-    dataFollowing.relationships_following.forEach((name,index) => {
-      const exist = data1.some(item => item === name['title'])
-      if(!exist) {
-        data2.push(name)
-      }
-    })
-    console.log(data2)
+output.innerHTML = ""
+
+dataFollowing.relationships_following.reverse().forEach((name) => {
+
+  const exist = data1.some(item => item === name.title)
+
+  if(!exist){
+
+    let tr = document.createElement('tr')
+
+    let tdNo = document.createElement('td')
+    tdNo.textContent = data2.length + 1
+
+    let tdAccount = document.createElement('td')
+    tdAccount.textContent = name.title
+
+    let tdDate = document.createElement('td')
+    
+    tdDate.textContent = new Date(name.string_list_data[0].timestamp * 1000).toLocaleString('id-ID', { 
+    timeZone: 'Asia/Jakarta', 
+    hour12: false})
+
+    tr.appendChild(tdNo)
+    tr.appendChild(tdAccount)
+    tr.appendChild(tdDate)
+
+    output.appendChild(tr)
+
+    data2.push(name)
+  }
+})
+    
+    
+      console.log(data2)
+    
 });
 
 
