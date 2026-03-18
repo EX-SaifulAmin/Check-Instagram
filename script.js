74b697f144e36b73907a8754a7ff5326aff4026a
Raja iblis <ytamingamers@gmail.com>
2026-03-18 16:34:28 +0700

    download table

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 // }
 const fileInput = document.getElementById("inputFile");
 let output = document.getElementById('output');
+let selectedFiles = null
 fileInput.addEventListener("change", async function(){
-    const file = this.files[0];
-    const zip = await JSZip.loadAsync(file);
+  selectedFiles = this.files[0]
+  console.log('siap diproses')
+})
+async function buatTable() {
+    if(!selectedFiles) return alert('Yaelah masukin filenya dulu dek')
+    const zip = await JSZip.loadAsync(selectedFiles);
     const jsonFollowing = zip.file("connections/followers_and_following/following.json");
     const jsonFollowers = zip.file("connections/followers_and_following/followers_1.json");
     if(!jsonFollowers|| !jsonFollowing ){
@@ -44,10 +49,27 @@ dataFollowing.relationships_following.reverse().forEach((name) => {
     tdDate.textContent = new Date(name.string_list_data[0].timestamp * 1000).toLocaleString('id-ID', { 
     timeZone: 'Asia/Jakarta', 
     hour12: false})
+    
+    let tdAction = document.createElement('td')
+    let btn = document.createElement('button')
+    btn.textContent = "Copy"
+    btn.style.cursor = "pointer"
+    tdAccount.appendChild(btn)
+    btn.addEventListener("click", () => {
+  navigator.clipboard.writeText(name.title)
+    .then(() => {
+      btn.textContent = "Copied! ✅"
+      setTimeout(()=>{
+        btn.textContent = "Copy"
+      },1500)
+    })
+})
 
+    
     tr.appendChild(tdNo)
     tr.appendChild(tdAccount)
     tr.appendChild(tdDate)
+  
 
     output.appendChild(tr)
 
@@ -57,7 +79,41 @@ dataFollowing.relationships_following.reverse().forEach((name) => {
     
     
       console.log(data2)
-    
-});
+}
+
+
+function tableToCSV(){
+  let rows = document.querySelectorAll("table tr")
+  let csv = []
+
+  rows.forEach(row => {
+    let cols = row.querySelectorAll("td, th")
+    let rowData = []
+
+    cols.forEach((col,index) => {
+      if(index !==0) {
+        rowData.push(col.innerText)
+      }
+    })
+
+    csv.push(rowData.join(","))
+  })
+
+  download(csv.join("\n"))
+}
+
+function download(data){
+  const blob = new Blob([data], { type: "text/csv" })
+  const url = URL.createObjectURL(blob)
+
+  const a = document.createElement("a")
+  const now = new Date()
+const fileName = `not_follow_back_${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}.csv`
+
+  a.download = fileName
+  a.href = url
+  a.click()
+}
+
 
 
